34ff84bf277048dec9d07674e6a31461c6aa0414
Younger Shen <younger.x.shen@gmail.com>
2017-09-11 15:02:24 +0800

    init

---
+++ validator/__init__.py
@@ -0,0 +1,4 @@
+# PROJECT : validator
+# TIME : 17-9-11 下午2:59
+# AUTHOR : Younger Shen
+# EMAIL : younger.x.shen@gmail.com
---
+++ validator/tests.py
@@ -0,0 +1,4 @@
+# PROJECT : validator
+# TIME : 17-9-11 下午3:00
+# AUTHOR : Younger Shen
+# EMAIL : younger.x.shen@gmail.com
