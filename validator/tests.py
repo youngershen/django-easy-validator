349e11a9d7085abac730aa63f7b9ae40dcd03b67
younger shen <younger.x.shen@gmail.com>
2017-10-20 23:15:59 +0800

    add accepted validator test case

--- validator/tests.py
+++ validator/tests.py
@@ -47,7 +47,18 @@ class Test(TestCase):
             'term': 'yes',
             'policy': 'yes'
         }
+        data_invalid1 = {
+            'term': '',
+            'policy': 'hello'
+        }
+        data_invalid2 = {
+            'term': 'hello',
+            'policy': 'hello'
+        }
+
         self.accepted_valid = AcceptedValidator(data_valid)
+        self.accepted_invalid1 = AcceptedValidator(data_invalid1)
+        self.accepted_invalid2 = AcceptedValidator(data_invalid2)
 
     def test_required(self):
         self.assertTrue(self.required_valid.validate())
@@ -57,3 +68,12 @@ class Test(TestCase):
 
     def test_accepted(self):
         self.assertTrue(self.accepted_valid.validate())
+        self.assertFalse(self.accepted_invalid1.validate())
+        message = self.accepted_invalid1.get_message()
+        self.assertDictEqual(message, {'term': {'required': 'term field is required'},
+                                       'policy': {'accepted': 'policy field must in which of : yes or no'}})
+        self.assertFalse(self.accepted_invalid2.validate())
+        message = self.accepted_invalid2.get_message()
+        print(message)
+        self.assertDictEqual(message, {'term': {'accepted': 'term field must in which of : yes or no'},
+                                       'policy': {'accepted': 'policy field must in which of : yes or no'}})
