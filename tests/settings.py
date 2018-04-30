3bba99723f5a1bf28c653e0c664ffaa2957b5f3d
younger shen <younger.shen@hotmail.com>
2018-04-30 17:57:13 +0800

    change test db to sqlite

--- tests/settings.py
+++ tests/settings.py
@@ -17,12 +17,8 @@ INSTALLED_APPS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hhcms',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'validators',
     }
 }
 
