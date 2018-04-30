982fb46869b9af8094412cfe17cc703d66ea57e4
younger shen <younger.shen@hotmail.com>
2018-04-30 18:08:56 +0800

    remove pymysql in tests/settings.py

--- tests/settings.py
+++ tests/settings.py
@@ -4,10 +4,6 @@
 # EMAIL : younger.shen@hotmail.com
 # CELL : 13811754531
 # WECHAT : 13811754531
-from pymysql import install_as_MySQLdb
-
-install_as_MySQLdb()
-
 SECRET_KEY = "@1o4_s8+lfapx2%c7azo6orns9p-o#9(b$96mkf#+3+kt1(gl_"
 
 INSTALLED_APPS = [
